dbfa7f36dd2ce022a6ec656b80b3dac06ec49b7e
ensar <ensarkarabudak@gmail.com>
2019-04-02 01:59:42 +0300

    guncelleme

--- dosya.py
+++ dosya.py
@@ -1,15 +1,30 @@
 sifreli_metin=''
-def getMessage():
-    return input()
+def desifrele(karakter):
+    if karakter.isalpha() or karakter.isspace():
+        sayi = ord(karakter)
+        sayi -= coz
+        if karakter.isupper():
+            if sayi > ord('Z'):      
+                sayi -= 26
+            elif sayi < ord('A'):
+                sayi += 26
+        elif karakter.islower():
+            if sayi > ord('z'):      
+                sayi -= 26
+            elif sayi < ord('a'):    
+                sayi += 26
+        print(chr(sayi))
+    else:
+        pass
 
 def sifrele(karakter):
-    if karakter.isalpha():
+    if karakter.isalpha() or karakter.isspace():
         sayi = ord(karakter)
         sayi += anahtar
         if karakter.isupper():
             if sayi > ord('Z'):      
                 sayi -= 26
-            elif sayi < ord('A'):    
+            elif sayi < ord('A'):
                 sayi += 26
         elif karakter.islower():
             if sayi > ord('z'):      
@@ -18,25 +33,34 @@ def sifrele(karakter):
                 sayi += 26
         dosya_yaz(chr(sayi))
     else:
-        print('KARAKTER HATASI')
+        pass
 
 def dosya_yaz(sifreli_karakter):
-    with open("sifreli_dosya.txt","a") as f:
+    with open("sifreli_dosya.txt","a+") as f:
         f.writelines(sifreli_karakter)
-       
 try:
     while True:
         print('Lufen bir anahtar girin(1-26):')
-        anahtar=int(getMessage())
+        anahtar=int(input())
         if(anahtar>0 and anahtar<=26):
             with open("sifrele.txt","r+") as dosya:
                 tut=dosya.readlines()
                 for symbol in tut:
-                    for i in range(0,len(symbol)):
-                        sifrele(symbol[i])       
+                    for i in symbol:
+                        sifrele(i) 
             break
+     
         else:
             print('LUTFEN 1-29 ARASINDA BIR SAYI GIRIN')
+
+    while True:
+        print('Anahtar Gir:')
+        coz=int(input())
+        with open("sifreli_dosya.txt","r+") as dosya:
+            tut=dosya.readlines()
+            for symbol in tut:
+                for i in symbol:
+                    desifrele(i)
+        break
 except ValueError:
     print("Lutfen sayı giriniz")
-
