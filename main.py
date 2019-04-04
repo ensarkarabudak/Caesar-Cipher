61b13adf06aa49323d6d0b04d0ea181fa975822f
ensar <ensarkarabudak@gmail.com>
2019-04-04 20:25:32 +0300

    Css Hata Giderildi

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
-		return render_template('index.html')
+	return render_template('index.html')
 
 @app.route("/sifrele")
 def sifrele():
