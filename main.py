649592bd186003df131945fa87ee2255ad2a0719
ensar <ensarkarabudak@gmail.com>
2019-04-04 20:16:23 +0300

    Sayfa Duzenleme

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
