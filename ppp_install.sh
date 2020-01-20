a4fe065be6ec467e899e3e8cd4cd902edc3622d1
Ensar <ensarkarabudak@gmail.com>
2020-01-20 13:24:48 +0300

    Update ppp_install.sh

--- ppp_install.sh
+++ ppp_install.sh
@@ -143,12 +143,13 @@ if ! (grep -q 'sudo route' /etc/ppp/ip-up ); then
     echo "sudo route add default ppp0" >> /etc/ppp/ip-up
 fi
 
+: '
 if [ $1 -eq 2 ]; then
 	if ! (grep -q 'max_usb_current' /boot/config.txt ); then
 		echo "max_usb_current=1" >> /boot/config.txt
 	fi
 fi
-
+'
 
 	
 #read auto_reconnect == $4
